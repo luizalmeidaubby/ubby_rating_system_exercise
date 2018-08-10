60b17e86cb118ed54f8b07d0fec46b8984f9c2db
Luíz Augusto Magalhães De Almeida <luizalmeida@Luizs-Mac-mini.local>
2018-08-10 17:01:51 -0300

    initial commit

---
+++ contracts/ProductRating.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract ProductRating is StandardToken, Ownable {
+
+    struct Product {
+        string description;
+        uint totalRating;
+        uint amountReviews;
+        mapping(address => bool) rated;
+    }
+
+    mapping(uint => Product) products;
+
+    //Default token properties
+    string public name = "ProductRating";
+    string public symbol = "PDR";
+    uint8 public decimals = 2;
+    uint public INITIAL_SUPPLY = 1000000;
+
+    constructor() public {
+        owner = msg.sender;
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
+
+    //Replace the product to be rated
+    function replace(string _description) public onlyOwner {
+        require(keccak256(bytes(_description)) != keccak256(""), "The product must have a description!");
+        require(msg.sender == owner, "Permission denied! Reason: This operation is allowed only to the owner");
+
+        Product memory product = Product({
+            description: _description,
+            totalRating: 0,
+            amountReviews: 0
+        });
+
+        products[0] = product;
+    }
+
+    //Review the product
+    function review(uint8 _userReview) public {
+        Product storage product = products[0];
+        require(_userReview >= 0 && _userReview <= 5, "The value of rating must be between 0 and 5");
+        require(!product.rated[msg.sender], "This address already rated the product");
+
+        product.amountReviews++;
+        product.totalRating += _userReview;
+    }
+
+    //Calculate the product rating and returns it and the product description
+    function get() public view returns (string description, uint average) {
+        Product storage product = products[0];
+
+        return (
+            product.description,
+            product.amountReviews > 0 ? (product.totalRating/product.amountReviews) : 0
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
